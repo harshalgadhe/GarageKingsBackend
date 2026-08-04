1d321eaa6301a834d21da7136681ac60df9a0145
harshalgadhe <harshalgadhe123@gmail.com>
2026-08-05 02:11:09 +0530

    fix(cloudfront): forward Lambda payload hash

--- terraform/main.tf
+++ terraform/main.tf
@@ -338,7 +338,7 @@ resource "aws_cloudfront_distribution" "cdn" {
 
     forwarded_values {
       query_string = true
-      headers      = ["X-Authorization", "Origin"]
+      headers      = ["X-Authorization", "X-Amz-Content-Sha256", "Origin"]
       cookies { forward = "all" }
     }
 
